32e45f20e7e0654e52c33b31567740622c702bdc
gjoedev <74843242+gjoedev@users.noreply.github.com>
2021-09-04 17:41:59 -0400

    stuff

--- commands/notifier.js
+++ commands/notifier.js
@@ -50,8 +50,8 @@ module.exports = {
                 } else {
                     if(contents[1] == 'edit'){
                         var tfile = contents[2]
-                        var tfilec = JSON.parse(fs.readFileSync('../notifier/' + tfile + '.json'))
-                        console.log(tfilec)
+                        var notifierdir = fs.readdirSync('../notifier/')
+                        console.log(notifierdir)
                     } else {
                         const newEmbed = new Discord.MessageEmbed() .setColor('#D82B00') .setTitle('Failure!') .setDescription('Failed to Use !notifier') .addFields( {name: 'Reason', value: 'Invalid syntax, do !notifier help for more'})
                         message.channel.send(newEmbed)
