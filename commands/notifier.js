be57c55fd07feb180580ff4fa24d99c1bc6fb41d
gjoe <74843242+gjoedev@users.noreply.github.com>
2021-09-05 09:56:28 -0400

    Add edit notifier feature to stable release (#1)
    More info in dev branch

--- commands/notifier.js
+++ commands/notifier.js
@@ -1,9 +1,10 @@
 const path = require('path')
+const fs = require('fs')
 
 module.exports = {
     name: 'notifier',
     description: 'Sets the message for an event',
-    execute(message, args, Discord, fs){
+    execute(message, args, Discord){
 
         const contents = message.content.split(/[ ,]/)
 
@@ -13,7 +14,6 @@ module.exports = {
         } else {
             if(contents[1] == 'add'){
                 const Name = contents[2]
-                var commandstr = contents[0] + contents[1] + contents[2]
                 var textstr = contents.slice(3).join(' ');
                 var notifierARR = {
                     'name' : Name,
@@ -48,8 +48,53 @@ module.exports = {
                         }
                     })
                 } else {
-                    const newEmbed = new Discord.MessageEmbed() .setColor('#D82B00') .setTitle('Failure!') .setDescription('Failed to Use !notifier') .addFields( {name: 'Reason', value: 'Invalid syntax, do !notifier help for more'})
-                    message.channel.send(newEmbed)
+                    if(contents[1] == 'edit'){
+                        if(contents[2] == undefined || contents[3] == undefined){
+                            const newEmbed = new Discord.MessageEmbed() .setColor('#D82B00') .setTitle('Failure!') .setDescription('Failed to edit notifier') .addFields( {name: 'Reason:', value: 'Invalid syntax, do !notifier help for'})
+                            message.channel.send(newEmbed)
+                            return;
+                        }
+                        var t = contents[2]
+                        console.log(t)
+                        try{
+                            var tfilee = fs.readFileSync('./notifier/' + t + '.json')
+                        } catch(err) {
+                            if(err.code == 'ENOENT'){
+                                const newEmbed = new Discord.MessageEmbed() .setColor('#D82B00') .setTitle('Failure!') .setDescription('Failed to edit notifier') .addFields( {name: 'Reason:', value: 'Nonexistant notifier, run !notifier help for list of available notifiers'})
+                                message.channel.send(newEmbed)
+                                return;
+                            }
+                        }
+                        var tfilee = fs.readFileSync('./notifier/' + t + '.json')
+                        console.log(tfilee)
+                        var tfile = JSON.parse(tfilee)
+                        console.log(tfile)
+                        fs.unlinkSync('./notifier/' + t + '.json', function(err){
+                            if(err){
+                                const newEmbed = new Discord.MessageEmbed() .setColor('#D82B00') .setTitle('Failure!') .setDescription('Failed to edit notifier') .addFields( {name: 'Error while removing file:', value: '```' + err.toString() + '```'})
+                                message.channel.send(newEmbed)
+                                return;
+                            }
+                        })
+                        var ntext = contents.slice('3').join(' ')
+                        var newarr = {
+                            'name' : tfile.name,
+                            'text' : ntext
+                        }
+                        var contentss =  JSON.stringify(newarr)
+                        fs.writeFileSync('./notifier/' + tfile.name + '.json', contentss, function(err){
+                            if(err){
+                                const newEmbed = new Discord.MessageEmbed() .setColor('#D82B00') .setTitle('Failure!') .setDescription('Failed to edit notifier') .addFields( {name: 'Error while writing file:', value: '```' + err.toString() + '```'})
+                                message.channel.send(newEmbed)
+                                return;
+                            }
+                        })
+                        const embed = new Discord.MessageEmbed().setColor('#12CB3C').setTitle('Succsess!').setDescription('Notifier edited Sucsessfully').addFields({name: 'Notifier Edited:', value: tfile.name}, {name: 'New Contents:', value: ntext})
+                        message.channel.send(embed)
+                    } else {
+                        const newEmbed = new Discord.MessageEmbed() .setColor('#D82B00') .setTitle('Failure!') .setDescription('Failed to Use !notifier') .addFields( {name: 'Reason', value: 'Invalid syntax, do !notifier help for more'})
+                        message.channel.send(newEmbed)
+                    }
                 }
             }
         }
